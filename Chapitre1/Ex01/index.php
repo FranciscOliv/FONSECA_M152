d71c01c1e5891dfcb6e40d98587c11c8a5d87a39
Francisco Oliveira <38466225+FranciscOliv@users.noreply.github.com>
2020-01-23 16:12:23 +0100

    changements

--- Chapitre1/Ex01/index.php
+++ Chapitre1/Ex01/index.php
@@ -84,22 +84,24 @@
         <div class="row">
             <section class="col bg-dark mr-4"> lol 1
 
-            <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div><div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div><div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top"  src="https://www.ikea.com/ch/fr/images/products/djungelskog-soft-toy__0710188_PE727391_S5.JPG" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://www.ikea.com/ch/fr/images/products/djungelskog-soft-toy__0710188_PE727391_S5.JPG" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top"  src="https://www.ikea.com/ch/fr/images/products/djungelskog-soft-toy__0710188_PE727391_S5.JPG" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
             </section>
             <section class="col bg-dark"> lol 2</section>
 
